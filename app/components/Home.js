f0cb8ee50a0f4c160a4593f63e02d95714d00aa9
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2024-05-30 14:33:22 -0500

    playwight test video added

--- app/components/Home.js
+++ app/components/Home.js
@@ -6,9 +6,9 @@ function Home() {
     <div className="container py-md-5">
       <h1>Mohammad Ziaur Rahman</h1>
       <div className="video-container">
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/2gksvPlRIP8" frameborder="0" allowfullscreen></iframe>
+        <iframe src="https://www.youtube.com/embed/2gksvPlRIP8" frameborder="0" allowfullscreen="ture"></iframe>
+        <iframe src="https://www.youtube.com/embed/E4ikuWxpu1M" frameborder="0" allowfullscreen="true"></iframe>
       </div>
-      {/* <img src={`${hero}`} className="responsive" id="hero-responsive" alt="laptop_img_hero" /> */}
       <p className="sdet" id="sdet-home">
         {" "}
         SDET with a proven record of automating user acceptance tests by leveraging coding skills. Proven ability to build, deploy, run, and manage individual applications. Accustomed to building test scenarios that reduce maintenance and software support cost. Utilizing Git for version control, Gitlab for CI/CD and JIRA for defect tracking. Proficient at liaising with scrum masters during daily stand-up meeting and sprint planning/review meetings to ensure technical deliverables are achieved.
