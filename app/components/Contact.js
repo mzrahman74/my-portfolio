f993e38a190fe1e6b7b6420a4a919b3bca092d94
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2024-06-14 21:12:56 -0500

    updated gitLab test for playwright api

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -5,6 +5,7 @@ function Contact() {
   return (
     <div>
       <img className="contact_me" height="200" id="contact-id" src={`${contact}`} alt="contact_me_img" />
+      <hr />
       <p className="note" id="note-id-1st">
         {" "}
         Please let me know if you have any questions about my background. I will be happy to answer all your questions.{" "}
