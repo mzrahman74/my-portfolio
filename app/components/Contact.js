ed321a2671e0bbeabc871df5efd178336d33ab8f
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2024-07-08 20:57:27 -0500

    updated email link contact page

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -14,7 +14,9 @@ function Contact() {
         Phone:(214) 973-0438{" "}
       </p>
       <p className="note" id="note-id-3rd">
-        Email: mzrahman74@gmail.com{" "}
+        {" "}
+        Email:
+        <a href="mailto:mzrahman74@gamil.com">mzrahman74@gmail.com </a>
       </p>
       <div className="map">
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d107436.84423414226!2d-97.21798558199373!3d32.70196040701056!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x864e62d2e40898d3%3A0xb5ef6ac1fa05351!2sArlington%2C%20TX!5e0!3m2!1sen!2sus!4v1720100984197!5m2!1sen!2sus" width="100%" height="100%" style={{ border: 0 }} allowFullscreen="true" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
