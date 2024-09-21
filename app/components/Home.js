0192f90af9613a0b107262eb55d21cc183d8a353
Mohammad Rahman <mohammadrahman@Mohammads-iMac.attlocal.net>
2024-09-20 20:38:05 -0500

    updated grid for videos

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,10 +8,10 @@ function Home() {
       <hr />
       <div className="video-container">
         <div className="video-row">
-          <iframe className="video_one" src="https://www.youtube.com/embed/L7dAYld_7aM" frameborder="0" allowfullscreen="ture"></iframe>
-          <iframe className="video_two" src="https://www.youtube.com/embed/E4ikuWxpu1M" frameborder="0" allowfullscreen="true"></iframe>
-          <iframe className="video_three" src="https://www.youtube.com/embed/ER9Er4P8TJE" frameborder="0" allowfullscreen="true"></iframe>
-          <iframe className="video_four" src="https://www.youtube.com/embed/jJuD65mdd9E" frameborder="0" allowfullscreen="true"></iframe>
+          <iframe className="video_one" id="video" src="https://www.youtube.com/embed/L7dAYld_7aM" frameborder="0" allowfullscreen="ture"></iframe>
+          <iframe className="video_two" id="video" src="https://www.youtube.com/embed/E4ikuWxpu1M" frameborder="0" allowfullscreen="true"></iframe>
+          <iframe className="video_three" id="video" src="https://www.youtube.com/embed/ER9Er4P8TJE" frameborder="0" allowfullscreen="true"></iframe>
+          <iframe className="video_four" id="vidoeo" src="https://www.youtube.com/embed/jJuD65mdd9E" frameborder="0" allowfullscreen="true"></iframe>
         </div>
         <div className="video-row">
           <iframe className="video_five" src="https://www.youtube.com/embed/uDkvctQyKNM" frameborder="0" allowfullscreen="true"></iframe>
